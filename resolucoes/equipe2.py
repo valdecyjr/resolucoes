078921d6d7a6e1125dea451adc416af44309a9b9
Valdecy Junior <valdecysjunior@gmail.com>
2022-12-05 11:54:54 -0300

    Add files via upload

---
+++ resolucoes/equipe2.py
@@ -0,0 +1,33 @@
+'''
+    Escreva um programa para ler uma senha e verificar se a mesma contém as exigências de segurança.Pelo menos 
+    um caractere maiúsculo e um minúsculo,além de conter números e letras, bem como contenha no minimo 4
+    e no maximo 8
+'''
+def verifsenha(senha):
+    numeros = False
+    maiusculo = False
+    minusculo = False
+    if len(senha)  >= 4 and len(senha) <= 8:
+        if senha.lower() != senha:
+            minusculo = True
+        else:
+            print('senha não contém letras minusculas')
+        if senha.upper() != senha:
+            maiusculo = True
+        else:
+            print('senha não contém letras maiúsculas')    
+        for letra in senha:
+            if letra.isnumeric():
+                numeros = True
+        if not numeros:
+            print('senha não contém numeros')
+        if maiusculo and minusculo and numeros:
+            print('senha segura')
+        else:
+            print('senha sem números')
+
+while True:
+    senha = input('digite uma senha: ') 
+    verifsenha(senha)
+    if senha == 'sair':
+        break            
---
+++ resolucoes/equipe6.py
@@ -0,0 +1,27 @@
+"""
+    Enunciado = Fazer um programa onde seja inserido nomes de alunos e a quantidade de equipes.
+Esses alunos tem que ser organizados na equipe com quantidades iguais.
+Obs: O divisão das equipes fica a seu critério
+"""
+
+
+def orgequipe(alunos):
+    equipe1, equipe2 = [], []
+    if len(alunos) % 2 == 0:
+        limiteinferior = int(len(alunos)/2)
+        limitesuperior = int(len(alunos))
+
+        for numero in range(limiteinferior):
+            equipe1.append(alunos[numero])
+        for numero in range(limiteinferior, limitesuperior):
+            equipe2.append(alunos[numero])
+
+        print(f'Equipe1: {equipe1}')
+        print(f'Equipe2: {equipe2}') 
+    else:
+        print('Não é possivel dividir de forma igual')
+alunos = []
+qnt_alunos = int(input('Digite a quantidade de alunos: '))
+while len(alunos) < qnt_alunos:
+    alunos.append(input('Digite o nome do aluno: '))
+orgequipe(alunos)
