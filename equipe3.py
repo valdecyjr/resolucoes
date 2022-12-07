1468310580a5bdd7b9742bba6a31141c3044801a
valdecyjr <valdecysjunior@gmail.com>
2022-12-07 08:05:06 -0300

    consertando o codigo questao3.py

--- equipe3.py
+++ equipe3.py
@@ -3,11 +3,13 @@
     se não for dígitos romanos, reporta um erro, e depois transforme esse número em cardinal
 """
 
+
 # Função que recebe o numero romano
 def decodificador(numero):
     # Dicionario com o simbolo em romano e seu valor
-    dic = {'I': 1, 'IV': 4, 'V': 5,'IX': 9, 'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
-           'CD':400, 'D': 500, 'CM': 900, 'M': 1000}
+    dic = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10,
+           'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
+           'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
 
     # Caracteres que aparecem junto com outros caracteres
     caracter_especiais = ["I", "X", "C"]
@@ -25,18 +27,18 @@ def decodificador(numero):
     flag = False
 
     # vai percorrer de 0 ate o tamanho da minha lista
-    for i in range(len(numero_romano)): 
+    for i in range(len(numero_romano)):
         # Se esse valor ainda nao foi contando
         if not flag:
             # verifico se ele ta no Dicionario
             if numero_romano[i] in dic:
                 # Se o caracter é um dos especiais
                 if numero_romano[i] in caracter_especiais and i < len(numero_romano)-1:
-                    # Se o que vem depois desse é um especial tambem
-                    # E ainda exista um caractere apos ele
+                    # Verifica se o caractere é um caractere especial
+                    # E se existe um caractere apos ele
                     # Vai concatenar eles
                     auxiliar = numero_romano[i] + numero_romano[i+1]
-                    # Se eles existem no meu dicionario vou pegar os valores dele
+                    # Se ele concatenado existe no meu dicionario vou pegar o valore dele
                     if auxiliar in dic:
                         # Pego o valor dele e efetuo a soma
                         valor += dic[auxiliar]
@@ -61,6 +63,7 @@ def decodificador(numero):
             flag = False
 
     print(valor)
+
 
 if __name__ == "__main__":
     numero = input('Digite um numero numero romano entre 0 e 3999(Ex: XIX): ')
